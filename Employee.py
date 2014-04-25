de407d1b3fc4f8f39ac7ca33193c61b7c2448fc4
charleswang007 <charleswang007@gmail.com>
2014-04-24 21:54:30 -0700

    added rank field

--- Employee.py
+++ Employee.py
@@ -4,13 +4,17 @@ class Employee:
    'Common base class for all employees'
    empCount = 0
 
-   def __init__(self, name, salary):
+   def __init__(self, name, salary, rank):
       self.name = name
       self.salary = salary
+      self.rank = rank
       Employee.empCount += 1
    
    def displayCount(self):
-     print "Total Employee %d" % Employee.empCount
+      print "Total Employee %d" % Employee.empCount
 
    def displayEmployee(self):
       print "Name : ", self.name,  ", Salary: ", self.salary
+
+   def getRank(self):
+      return self.rank
--- hello.py
+++ hello.py
@@ -3,10 +3,16 @@ import unittest
 print "testing the Employee class!"
 
 class BasicTest(unittest.TestCase):
+    
     def test_total_employee_count(self):
-        emp1 = Employee("Alice", 2000)
-        emp2 = Employee("Bob", 5000)
-        self.assertEqual(Employee.empCount, 2)
+        emp1 = Employee("Alice", 2000, 3)
+        emp2 = Employee("Bob", 5000, 5)
+        self.assertEqual(Employee.empCount, 4)
+
+    def test_employee_rank(self):
+        emp1 = Employee("Charlie", 3000, 3)
+        emp2 = Employee("Deborah", 4000, 5)
+        self.assertTrue(emp1.getRank() < emp2.getRank())
 
 if __name__ == '__main__':
     unittest.main() 
